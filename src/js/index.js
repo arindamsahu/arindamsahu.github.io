9f4d8b5077fdaa767d142534c3b79695aa9a2754
axs3ix2 <arindam_sahu@homedepot.com>
2020-11-18 13:07:11 -0600

    Revert "Add script async"
    This reverts commit 412092a249561bceef20d060d65f0397679054b2.

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,55 @@
 window.addEventListener("scroll", onscroll);
 
+window.addEventListener("DOMContentLoaded", function() {
 
-// get the form elements defined in your form HTML above
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
 
-var form = document.getElementById("my-form");
-var button = document.getElementById("my-form-button");
-
-// Success and Error functions for after the form is submitted
-
-function success() {
-  form.reset();
-  button.style = "display: none ";
-  document.getElementById("my-form-status").innerHTML = "Thanks for your e-email. I'll reply back as soon as I can !";
-}
-
-function error() {
-  document.getElementById("my-form-status").innerHTML = "Oops! There was a problem.";
-}
-
-// handle the form submission event
-
-form.addEventListener("submit", function (ev) {
-  ev.preventDefault();
-  var data = new FormData(form);
-  ajax(form.method, form.action, data, success, error);
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks for your e-email. I'll reply back as soon as I can !";
     }
-  };
-  xhr.send(data);
-}
 
-function onscroll() {
-  if (window.pageYOffset >= document.getElementsByTagName('header')[0].offsetTop) {
-    document.getElementById('header').classList.add('sticky');
-  } else {
-    document.getElementById('header').classList.remove('sticky');
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
   }
-}
+
+function onscroll(){
+    if (window.pageYOffset >= document.getElementsByTagName('header')[0].offsetTop) {
+            document.getElementById('header').classList.add('sticky');
+        } else {
+            document.getElementById('header').classList.remove('sticky');
+        }
+  }
